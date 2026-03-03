684b71c096619369f22e9c6a0adc49166ccdd27d
auto premium <comercialautopremium@gmail.com>
2026-03-03 11:48:59 -0500

    se cambio el require auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,17 @@
 // Middleware: require login
 const requireAuth = (req, res, next) => {
   if (!req.session.user) {
+
+    // Si es una petición API (fetch / ajax)
+    if (req.originalUrl.startsWith('/api')) {
+      return res.status(401).json({ error: 'No autorizado' });
+    }
+
+    // Si es una vista normal
     req.session.returnTo = req.originalUrl;
     return res.redirect('/auth/login');
   }
+
   next();
 };
 
